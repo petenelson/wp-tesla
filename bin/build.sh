bbf9dca52fd77ddfa1c19851c4edd056a487a3a2
Pete Nelson <pete@petenelson.com>
2020-04-20 18:57:45 -0500

    initial circleci deployment

---
+++ bin/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Print commands to the screen
+set -x
+
+# Catch Errors
+set -euo pipefail
+
+sudo apt update # PHP CircleCI 2.0 Configuration File# PHP CircleCI 2.0 Configuration File sudo apt install zlib1g-dev libsqlite3-dev
+
+composer install
+
+# Stop printing commands to screen
+set +x
