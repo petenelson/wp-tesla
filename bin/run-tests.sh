58d9349cff8e9699eda02d7a23072ef7eabd9d92
Pete Nelson <pete@petenelson.com>
2020-05-16 16:35:45 -0500

    updated tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -13,6 +13,9 @@ cat /tmp/wordpress/wp-tests-config.php
 # Verify mysql
 sudo apt update
 sudo apt install -y default-mysql-client php-mysql
+
+php -i
+
 mysql -e 'show databases;' -h 127.0.0.1 -uroot --password=""
 
 ./vendor/bin/phpunit --verbose
