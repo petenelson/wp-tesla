2baab8f1e68e79c5f3ce9671b31d8ba07755d5b3
Pete Nelson <pete@petenelson.com>
2020-05-16 15:44:07 -0500

    updated install-tests

--- bin/install-tests.sh
+++ bin/install-tests.sh
@@ -29,7 +29,7 @@ sudo apt update
 sudo apt install -y default-mysql-client
 
 # Create WordPress database.
-mysql -e 'CREATE DATABASE wordpress_test;' -hlocalhost -uroot --password=""
+mysql -e 'CREATE DATABASE wordpress_test;' --h 127.0.0.1 -uroot --password=""
 
 # Switch back to the plugin dir
 popd
