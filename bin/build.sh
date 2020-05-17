8619614af2f4070732a2adce2dc160735afb8698
Pete Nelson <pete@petenelson.com>
2020-05-17 17:49:46 -0500

    updates tests

--- bin/build.sh
+++ bin/build.sh
@@ -6,8 +6,6 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-sudo apt update # PHP CircleCI 2.0 Configuration File# PHP CircleCI 2.0 Configuration File sudo apt install zlib1g-dev libsqlite3-dev
-
 composer install
 npm install
 npm run build
