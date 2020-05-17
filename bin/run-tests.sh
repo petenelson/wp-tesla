fdecc601755dab884329de25a2190446f80be662
Pete Nelson <pete@petenelson.com>
2020-05-17 17:17:51 -0500

    updates tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -10,13 +10,7 @@ export WP_DEVELOP_DIR=/tmp/wordpress/
 
 cat /tmp/wordpress/wp-tests-config.php
 
-
 # Verify mysql
-sudo apt update
-sudo apt install -y default-mysql-client
-
-php -i
-
 mysql -e 'show databases;' -h 127.0.0.1 -uroot --password=""
 
 ./vendor/bin/phpunit --verbose
