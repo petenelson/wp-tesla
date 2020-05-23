f373dbba661d2ed27f15d708cfc3f10e31d2b43b
Pete Nelson <pete@petenelson.com>
2020-05-23 18:13:38 -0500

    some debugging code for updating charge state

--- partials/vehicle-content.php
+++ partials/vehicle-content.php
@@ -10,6 +10,10 @@ use \WPTesla\Vehicle;
 $vehicle_id      = false;
 $battery_level   = false;
 $estimated_range = false;
+$updated         = false;
+$now             = time();
+
+$date_format     = get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
 
 if ( is_singular( \WPTesla\PostTypes\Tesla\get_post_type_name() ) ) {
 
@@ -30,6 +34,14 @@ if ( is_singular( \WPTesla\PostTypes\Tesla\get_post_type_name() ) ) {
 		// We'll look into km later.
 		$estimated_range = $estimated_range . 'mi';
 	}
+
+	$last_updated_ts = absint( get_post_meta( $vehicle->ID, Vehicle\get_charge_state_updated_key(), true ) );
+
+	$last_updated = new \DateTime( 'now', wp_timezone() );
+	$last_updated->setTimestamp( $last_updated_ts );
+
+	$next_update = new \DateTime( 'now', wp_timezone() );
+	$next_update->setTimestamp( $last_updated_ts + Vehicle\get_charge_sync_interval() );
 }
 
 ?>
@@ -42,6 +54,21 @@ if ( is_singular( \WPTesla\PostTypes\Tesla\get_post_type_name() ) ) {
 		</li>
 		<li>
 			<?php esc_html_e( 'Estimated Range', 'wp-tesla' ); ?>: <?php echo esc_html( $estimated_range ); ?>
+		</li>
+		<li>
+			<?php esc_html_e( 'Last Updated', 'wp-tesla' ); ?>: <?php echo esc_html( $last_updated->format( $date_format ) ); ?>
+		</li>
+		<li>
+			<?php esc_html_e( 'Next Update', 'wp-tesla' ); ?>: <?php echo esc_html( $next_update->format( $date_format ) ); ?>
+		</li>
+		<li>
+			<?php esc_html_e( 'Last Updated TS', 'wp-tesla' ); ?>: <?php echo esc_html( $last_updated_ts ); ?>
+		</li>
+		<li>
+			<?php esc_html_e( 'Now', 'wp-tesla' ); ?>: <?php echo esc_html( $now ); ?>
+		</li>
+		<li>
+			<?php esc_html_e( 'Diff', 'wp-tesla' ); ?>: <?php echo esc_html( $now - $last_updated_ts ); ?>
 		</li>
 	</ul>
 
