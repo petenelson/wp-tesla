4cc9fe4a86e5a33fddc6a6efeb8eae657872960b
Pete Nelson <pete@petenelson.com>
2021-01-11 14:08:26 -0600

    added nodejs npm

--- bin/install-tests.sh
+++ bin/install-tests.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 sudo -E docker-php-ext-install mysqli
 sudo apt-get update
-sudo apt-get install default-mysql-client
+sudo apt-get install default-mysql-client nodejs npm
 
 # Set up WordPress installation.
 export WP_DEVELOP_DIR=/tmp/wordpress/
