59f962c0d42b550771e24c5c70656a3b18ce9e4e
Pete Nelson <pete@petenelson.com>
2023-05-22 20:19:26 -0500

    try installing nvm

--- bin/build.sh
+++ bin/build.sh
@@ -8,6 +8,8 @@ set -euo pipefail
 
 composer install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use 16
 npm install
 npm run build
