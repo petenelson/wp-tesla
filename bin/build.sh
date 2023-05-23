0409fdd005af5dbf709a90206455d0128d59b27f
Pete Nelson <pete@petenelson.com>
2023-05-22 20:20:30 -0500

    install node 16

--- bin/build.sh
+++ bin/build.sh
@@ -10,6 +10,7 @@ composer install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 16
 nvm use 16
 npm install
 npm run build
